5d1945be0e932c06d562eeb248a69119bd378b01
LeoToti <Londra27?>
2021-05-25 16:39:00 +0200

    Changes

--- src/lib/fs-tools.js
+++
@@ -1,9 +0,0 @@
-import fs from "fs-extra"
-import { fileURLToPath } from "url"
-import { dirname, join } from "path"
-
-const { readJSON } = fs
-
-const dataFolderPath = join(dirname(fileURLToPath(import.meta.url)), "../data")
-
-export const getBooks = async () => await readJSON(join(dataFolderPath, "books.json"))
--- src/server.js
+++ src/server.js
@@ -1,43 +1,46 @@
 import express from "express"
-import listEndpoints from "express-list-endpoints"
 import cors from "cors"
+import {join, dirname} from "path"
+import {fileURLToPath} from "url"
+import listEndpoints from "express-list-endpoints"
+import userRoutes from "./authors/authors.js"
+import postRoutes from "./blogPosts/blogPosts.js"
+import {badRequestErrorHandler, notFoundErrorHandler, forbiddenErrorHandler, catchAllErrorHandler} from "./errorHandlers.js"
 
-import booksRouter from "./books/index.js"
+const publicFolder = join(dirname(fileURLToPath(import.meta.url)), "../public")
 
 const server = express()
-
 const port = process.env.PORT || 3001
 
-// MIDDLEWARES
-
-const whitelist = [process.env.FRONTEND_DEV_URL, process.env.FRONTEND_CLOUD_URL]
+const whiteList = [process.env.FRONTEND_DEV_URL, process.env.FRONTEND_CLOUD_URL]
 
 const corsOptions = {
-  origin: function (origin, next) {
-    console.log("ORIGIN ", origin)
-    if (whitelist.indexOf(origin) !== -1) {
-      // origin allowed
-      next(null, true)
-    } else {
-      // origin not allowed
-      next(new Error("CORS TROUBLES!!!!!"))
+    origin: function (origin, next) {
+
+        if(whiteList.indexOf(origin) !== -1) {
+            next(null, true)
+        } else {
+        next(new Error("Origin problem"))
+        }
     }
-  },
 }
-// CORS -> Cross Origin Resource Sharing
-// Different Origins --> http://domain-a.com different than http://domain-b.com
-// Different Origins --> http://localhost:3000 different than http://localhost:3001
-// Different Origins --> http://domain-a.com different than https://domain-a.com
-server.use(cors(corsOptions))
+
 server.use(express.json())
+server.use(express.static(publicFolder))
+server.use(cors(corsOptions))
 
-// ROUTES
-server.use("/books", booksRouter)
+server.use("/authors", userRoutes)
+server.use("/blogposts", postRoutes)
 
-// ERROR HANDLERS
+server.use(badRequestErrorHandler)
+server.use(notFoundErrorHandler)
+server.use(forbiddenErrorHandler)
+server.use(catchAllErrorHandler)
+
+server.listen(port, () => {
+    console.log("server is running port: ", port)
+})
 
 console.table(listEndpoints(server))
 
-server.listen(port, () => {
-  console.log(`Server listening on port: ${port}`)
-})
+
